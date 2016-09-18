02bb985b995c76a9109bfdb00f1687a498e1a796
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-18 10:58:39 -0500

    Errors in KeyPress events

--- Wallpaper_Warehouse/Form1.Designer.vb
+++ Wallpaper_Warehouse/Form1.Designer.vb
@@ -22,18 +22,171 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtLength = New System.Windows.Forms.TextBox()
+        Me.txtWidth = New System.Windows.Forms.TextBox()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.txtRollCoverage = New System.Windows.Forms.TextBox()
+        Me.lblTotalRolls = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(240, 237)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'txtLength
+        '
+        Me.txtLength.Location = New System.Drawing.Point(291, 33)
+        Me.txtLength.Name = "txtLength"
+        Me.txtLength.Size = New System.Drawing.Size(100, 20)
+        Me.txtLength.TabIndex = 1
+        '
+        'txtWidth
+        '
+        Me.txtWidth.Location = New System.Drawing.Point(397, 33)
+        Me.txtWidth.Name = "txtWidth"
+        Me.txtWidth.Size = New System.Drawing.Size(100, 20)
+        Me.txtWidth.TabIndex = 2
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Location = New System.Drawing.Point(503, 33)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(100, 20)
+        Me.txtHeight.TabIndex = 3
+        '
+        'txtRollCoverage
+        '
+        Me.txtRollCoverage.Location = New System.Drawing.Point(291, 123)
+        Me.txtRollCoverage.Name = "txtRollCoverage"
+        Me.txtRollCoverage.Size = New System.Drawing.Size(100, 20)
+        Me.txtRollCoverage.TabIndex = 4
+        '
+        'lblTotalRolls
+        '
+        Me.lblTotalRolls.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalRolls.Location = New System.Drawing.Point(503, 120)
+        Me.lblTotalRolls.Name = "lblTotalRolls"
+        Me.lblTotalRolls.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotalRolls.TabIndex = 5
+        Me.lblTotalRolls.Text = " "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(500, 107)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Single rolls:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(288, 107)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "&Roll Coverage:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(500, 17)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(68, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "&Height (feet):"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(394, 17)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(65, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "&Width (feet):"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(288, 17)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(70, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "&Length (feet):"
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(334, 198)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 11
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(503, 198)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 12
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(614, 261)
+        Me.ClientSize = New System.Drawing.Size(673, 261)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblTotalRolls)
+        Me.Controls.Add(Me.txtRollCoverage)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.txtWidth)
+        Me.Controls.Add(Me.txtLength)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Name = "frmMain"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Wallpaper Warehouse"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents txtLength As TextBox
+    Friend WithEvents txtWidth As TextBox
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents txtRollCoverage As TextBox
+    Friend WithEvents lblTotalRolls As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
 End Class
--- Wallpaper_Warehouse/Form1.vb
+++ Wallpaper_Warehouse/Form1.vb
@@ -1,3 +1,116 @@
 ﻿Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'Closes the app:
+        Me.Close()
+
+    End Sub
+
+    Private Function SingleRollsNeeded(ByVal dblL As Double, ByVal dblW As Double,
+                                       ByVal dblH As Double, ByVal dblRC As Double)
+
+        'Declares local variable:
+        Dim dblArea As Double
+        Dim dblSRN As Double
+
+        'Calculates:
+        dblArea = dblL * dblW * dblH
+        dblSRN = dblArea / dblRC
+
+        Return dblSRN
+
+
+    End Function
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+
+        'Declares variables:
+        Dim dblLength As Double
+        Dim dblWidth As Double
+        Dim dblHeigth As Double
+        Dim dblRollCoverage As Double
+        Dim dblSingleRollsNeeded As Double
+
+        'Assigns variables to text boxes:
+        Double.TryParse(txtLength.Text, dblLength)
+        Double.TryParse(txtWidth.Text, dblWidth)
+        Double.TryParse(txtHeight.Text, dblHeigth)
+        Double.TryParse(txtRollCoverage.Text, dblRollCoverage)
+
+        'Sends to function for calculation:
+        dblSingleRollsNeeded = SingleRollsNeeded(dblLength, dblWidth, dblHeigth, dblRollCoverage)
+
+        lblTotalRolls.Text = dblSingleRollsNeeded.ToString("N0")
+
+    End Sub
+
+    Private Sub txtLength_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtLength.KeyPress
+
+        'Allows only numbers, ".", and the backspace key in the text boxes:
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
+                e.KeyChar <> "." AndAlso
+                e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtLength_TextChanged(ByVal sender As Object, ByVal e As KeyPressEventArgs) Handles txtLength.TextChanged
+
+        'Clears the label:
+        lblTotalRolls.Text = ""
+
+    End Sub
+
+    Private Sub txtWidth_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtWidth.KeyPress
+
+        'Allows only numbers, ".", and the backspace key in the text boxes:
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
+                e.KeyChar <> "." AndAlso
+                e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtWidth_TextChanged(ByVal sender As Object, ByVal e As KeyPressEventArgs) Handles txtWidth.TextChanged
+
+        'Clears the label:
+        lblTotalRolls.Text = ""
+
+    End Sub
+
+    Private Sub txtHeight_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtHeight.KeyPress
+
+        'Allows only numbers, ".", and the backspace key in the text boxes:
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
+                e.KeyChar <> "." AndAlso
+                e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtHeight_TextChanged(ByVal sender As Object, ByVal e As KeyPressEventArgs) Handles txtHeight.TextChanged
+
+        'Clears the label:
+        lblTotalRolls.Text = ""
+
+    End Sub
+
+    Private Sub txtRollCoverage_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtRollCoverage.KeyPress
+
+        'Allows only numbers, ".", and the backspace key in the text boxes:
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
+                e.KeyChar <> "." AndAlso
+                e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtRollCoverage_TextChanged(ByVal sender As Object, ByVal e As KeyPressEventArgs) Handles txtRollCoverage.TextChanged
+
+        'Clears the label:
+        lblTotalRolls.Text = ""
+
+    End Sub
+
 
 End Class
